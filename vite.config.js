9d38123e0c868d9f8bbc1762def05d435d7c4100
Dánae De la Cruz <danae_taiyaki@hotmail.com>
2026-08-11 14:24:48 -0600

    Prepare application for production deployment

--- vite.config.js
+++ vite.config.js
@@ -2,28 +2,29 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
+    root: "src",
+    publicDir: "../public",
 
-  build: {
-    outDir: "../dist",
+    build: {
+        outDir: "../dist",
 
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        game: resolve(__dirname, "src/game/index.html"),
-      },
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "src/index.html"),
+                game: resolve(__dirname, "src/game/index.html"),
+            },
+        },
     },
-  },
 
-  server: {
-    port: 5991,
-    open: true,
+    server: {
+        port: 5991,
+        open: true,
 
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
+        proxy: {
+            "/api": {
+                target: "http://localhost:3000",
+                changeOrigin: true,
+            },
+        },
     },
-  },
 });
